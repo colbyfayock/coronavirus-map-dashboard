46c13cae4170249edd380b740dead974f31991a1
Colby Fayock <fayock@gmail.com>
2020-03-28 16:27:44 -0400

    boxshadow and map id

--- src/data/map-services.js
+++ src/data/map-services.js
@@ -10,6 +10,6 @@ export const mapServices = [
     name: 'Mapbox',
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    url: `https://api.mapbox.com/styles/v1/colbyfayock/ck8c1i80f0wfx1ir6qvxgfxj8/tiles/256/{z}/{x}/{y}@2x?access_token=${accessToken}`
+    url: `https://api.mapbox.com/styles/v1/colbyfayock/ck8c2foj72lqk1jnug0g2haw0/tiles/256/{z}/{x}/{y}@2x?access_token=${accessToken}`
   }
 ];
